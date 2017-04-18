3fc000acf1147e532417f04a563bfb5e7ba3be78
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-18 16:29:16 -0400

    First pass formula for Telepresence.

---
+++ Formula/telepresence.rb
@@ -0,0 +1,18 @@
+class Telepresence < Formula
+  desc "Local development environment attached to a remote Kubernetes cluster"
+  homepage "http://telepresence.io"
+  url "https://github.com/datawire/telepresence/archive/0.29.tar.gz"
+  sha256 "120cdf85e0ffafea317b28868d689dae4f4ed66208c495ba6d706b9adbdd26d7"
+
+  depends_on "python3"
+  depends_on "torsocks"
+  depends_on "homebrew/fuse/sshfs"
+
+  def install
+    bin.install "cli/telepresence"
+  end
+
+  test do
+    system "telepresence", "--help"
+  end
+end
