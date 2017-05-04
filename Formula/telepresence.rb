a9ab496b44edbe68426557d8c7fcf8ffbe6b1ca3
Travis CI User <travis@example.org>
2017-05-04 14:35:49 +0000

    Release 0.44

--- Formula/telepresence.rb
+++ Formula/telepresence.rb
@@ -3,8 +3,8 @@
 class Telepresence < Formula
   desc "Local development environment attached to a remote Kubernetes cluster"
   homepage "http://telepresence.io"
-  url "https://github.com/datawire/telepresence/archive/0.43.tar.gz"
-  sha256 "ad4b7b4d088cd3f2cd4d187276b27776190cc9f591705d8cabbb1903e78c7f15"
+  url "https://github.com/datawire/telepresence/archive/0.44.tar.gz"
+  sha256 "cb70c499462def27e1308fbd5e47c255cf6d818636cadefda2736d9bad8d5b02"
 
   depends_on "python3"
   depends_on "torsocks"
