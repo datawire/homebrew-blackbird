f9ef47f2a86544376cc2a6eeefdd0c5d17c4d3da
Travis CI User <travis@example.org>
2017-06-07 16:04:43 +0000

    Release 0.49

--- Formula/telepresence.rb
+++ Formula/telepresence.rb
@@ -3,18 +3,21 @@
 class Telepresence < Formula
   desc "Local development environment attached to a remote Kubernetes cluster"
   homepage "http://telepresence.io"
-  url "https://github.com/datawire/telepresence/archive/0.48.tar.gz"
-  sha256 "9e2d6b042758f47f0b6ec985fde82d2b9039363d68794eedaa5aa8b974790fdd"
+  url "https://github.com/datawire/telepresence/archive/0.49.tar.gz"
+  sha256 "dcf92beae8b6f5a7d8907998ff7c988b1c60f4abf1ba62acf0daee68dd6d2331"
 
   depends_on "python3"
   depends_on "torsocks"
   depends_on "sshfs"
 
   def install
+    system "env", "PATH=#{ENV['PATH']}:/usr/local/bin", "make", "virtualenv/bin/sshuttle-telepresence"
     bin.install "cli/telepresence"
+    bin.install "virtualenv/bin/sshuttle-telepresence"
   end
 
   test do
     system "telepresence", "--help"
+    system "sshuttle-telepresence", "--version"
   end
 end
