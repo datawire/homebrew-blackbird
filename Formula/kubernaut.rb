0dde3c340b525193017c88e315a45778773eae56
Philip Lombardi <plombardi@datawire.io>
2017-08-16 11:28:18 -0400

    fix homebrew: attempt 2

--- Formula/kubernaut.rb
+++ Formula/kubernaut.rb
@@ -12,7 +12,7 @@ class Kubernaut < Formula
 
   def install
     venv = virtualenv_create(libexec, "python3")
-    venv.pip_install_and_link
+    venv.pip_install_and_link buildpath
   end
 
   test do
