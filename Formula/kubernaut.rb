6da229d5f53d95f03420f4be5adfe01321f54795
Travis CI User <travis@example.org>
2017-08-16 14:56:41 +0000

    Release 0.1.8

--- Formula/kubernaut.rb
+++ Formula/kubernaut.rb
@@ -1,15 +1,21 @@
 # This script is generated automatically by the release automation code in the
 # Kubernaut repository:
 class Kubernaut < Formula
-  desc "On-demand, ephemeral Kubernetes clusters for development and testing"
+  include Language::Python::Virtualenv
+
+  desc     "On-demand, ephemeral Kubernetes clusters for development and testing"
   homepage "https://github.com/datawire/kubernaut"
-  url "https://github.com/datawire/kubernaut/archive/0.1.4.tar.gz"
-  sha256 "58f5b1ee61317ea39d0b380eec6727b58e59f21efdee2dcdbcaa09be6380ecb8"
+  url      "https://github.com/datawire/kubernaut/archive/0.1.8.tar.gz"
+  sha256   "61ac20db236df1a890203c87fa1032ea8d0bdaf0a138e0f3dac7d4c49e72e711"
 
   depends_on "python3"
 
   def install
-    bin.install "out/kubernaut"
+    venv = virtualenv_create(libexec)
+    system libexec/"bin/pip3", "install", "-v", "--no-binary", ":all:",
+                              "--ignore-installed", buildpath
+    system libexec/"bin/pip3", "uninstall", "-y", name
+    venv.pip_install_and_link buildpath
   end
 
   test do
