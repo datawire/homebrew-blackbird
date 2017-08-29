686de92d4b91cdb0aebe336cf5cb685c45ce4afb
Travis CI User <travis@example.org>
2017-08-29 22:38:07 +0000

    Release 0.65

--- Formula/telepresence.rb
+++ Formula/telepresence.rb
@@ -1,20 +1,25 @@
 # This script is generated automatically by the release automation code in the
 # Telepresence repository:
 class Telepresence < Formula
-  desc "Local development environment attached to a remote Kubernetes cluster"
-  homepage "http://telepresence.io"
-  url "https://github.com/datawire/telepresence/archive/0.64.tar.gz"
-  sha256 "118399f43dd09c5bbee9ac773d184a9d867bf7d20d461eb7de0f966af4a64722"
+  desc "Local dev environment attached to a remote Kubernetes cluster"
+  homepage "https://telepresence.io"
+  url "https://github.com/datawire/telepresence/archive/0.65.tar.gz"
+  sha256 "0a439ad1ac55c7d26e8169b0ab9bde35456829eca5854c14f9092c2645442560"
 
   depends_on "python3"
-  depends_on "torsocks"
-  depends_on "sshfs"
-  depends_on "socat"
+  depends_on "torsocks" => :run
+  depends_on "sshfs" => :run
 
   def install
-    system "env", "PATH=#{ENV['PATH']}:/usr/local/bin", "make", "virtualenv/bin/sshuttle-telepresence"
+    system "env", "PATH=#{ENV["PATH"]}:/usr/local/bin", "make", "virtualenv/bin/sshuttle-telepresence"
     bin.install "cli/telepresence"
     bin.install "virtualenv/bin/sshuttle-telepresence"
+  end
+
+  def caveats; <<-EOS.undent
+    Use of the container method requires socat.
+      brew install socat
+    EOS
   end
 
   test do
