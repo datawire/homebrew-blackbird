6bd9d2fcf394d645a033c1a4f3b54addb0d61524
d6e automaton <services@datawire.io>
2018-12-06 19:33:25 +0000

    Release 0.95

--- Formula/telepresence.rb
+++ Formula/telepresence.rb
@@ -4,8 +4,8 @@ class Telepresence < Formula
   include Language::Python::Virtualenv
   desc "Local dev environment attached to a remote Kubernetes cluster"
   homepage "https://telepresence.io"
-  url "https://s3.amazonaws.com/datawire-static-files/telepresence/telepresence-0.94.tar.gz"
-  sha256 "b607d67bb5b10ecb63a50b477aff8f1df95a35acd7e0d480181ceffc3d664d9e"
+  url "https://s3.amazonaws.com/datawire-static-files/telepresence/telepresence-0.95.tar.gz"
+  sha256 "619286709e174c8e6a7afb2627eb7639a3246bc1603d20217edb6f06bb22efd7"
 
   depends_on "python"
   depends_on "torsocks"
@@ -14,13 +14,6 @@ class Telepresence < Formula
   def install
     bin.install "bin/telepresence"
     libexec.install Dir["libexec/*"]
-  end
-
-  def caveats
-    <<~EOS
-      Use of the container method requires socat.
-        brew install socat
-    EOS
   end
 
   test do
