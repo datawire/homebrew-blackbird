1797069a468a4b056edbfcaf63e9ac665470cdaf
d6e automaton <services@datawire.io>
2020-09-10 13:47:44 +0000

    Release 0.108

--- Formula/telepresence.rb
+++ Formula/telepresence.rb
@@ -4,8 +4,8 @@ class Telepresence < Formula
   include Language::Python::Virtualenv
   desc "Local dev environment attached to a remote Kubernetes cluster"
   homepage "https://telepresence.io"
-  url "https://s3.amazonaws.com/datawire-static-files/telepresence/telepresence-0.107.tar.gz"
-  sha256 "9cfb062fd195f4c5afa99091b3e454f1c08a9aadf3d3a0304e8bdcc91ee86199"
+  url "https://s3.amazonaws.com/datawire-static-files/telepresence/telepresence-0.108.tar.gz"
+  sha256 "8e0df74749dac5a9770ea88060e021826a8c911b82fbbec2b7eea94aef5e125b"
 
   depends_on "python"
   depends_on "torsocks"
