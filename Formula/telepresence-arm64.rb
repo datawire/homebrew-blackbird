43447b56b497d0a9574d8a94ed05876c6b4db72b
d6e automaton <services@datawire.io>
2021-11-02 11:48:37 -0400

    Release 2.4.6

---
+++ Formula/telepresence-arm64.rb
@@ -0,0 +1,21 @@
+# This script is generated automatically by the release automation code in the
+# Telepresence repository:
+class TelepresenceArm64 < Formula
+  desc "Local dev environment attached to a remote Kubernetes cluster"
+  homepage "https://telepresence.io"
+  url "https://app.getambassador.io/download/tel2/darwin/arm64/2.4.6/telepresence"
+  sha256 "1b1e1b02f048d141ad2c65cfd5b6cf393572be2daaa783cc9a3e7aa2755aed3c"
+
+  # macfuse is a cask and formula can't depend on casks, so we can't actually
+  # do this. This is probably fine since you don't _need_ macfuse to run
+  # the cli, just to do mounts 
+  #depends_on "macfuse"
+
+  def install
+    bin.install "telepresence"
+  end
+
+  test do
+    system "#{bin}/telepresence", "--help"
+  end
+end
