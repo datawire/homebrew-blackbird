2d980fc1cc6aadb4b513dee6db423cc60980250f
d6e automaton <services@datawire.io>
2023-12-27 15:32:08 +0000

    Release 2.18.1

--- Formula/telepresence.rb
+++ Formula/telepresence.rb
@@ -3,8 +3,8 @@
 class Telepresence < Formula
   desc "Local dev environment attached to a remote Kubernetes cluster"
   homepage "https://telepresence.io"
-  url "https://app.getambassador.io/download/tel2/darwin/amd64/2.18.0/telepresence"
-  sha256 "bb88895e21c3a5d5392e93681586cc3877e4e2dc18c9ef5a1db49fa204e7d5c9"
+  url "https://app.getambassador.io/download/tel2/darwin/amd64/2.18.1/telepresence"
+  sha256 "606b806102cdfe4ca969de8a45979544e6a6b1ffdd0179f5e8f06157a28a228c"
 
   # macfuse is a cask and formula can't depend on casks, so we can't actually
   # do this. This is probably fine since you don't _need_ macfuse to run
