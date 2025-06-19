7975b9cfdee519cc68a3f99c1c60d54fb7f53338
d6e-automaton <dev@datawire.io>
2025-06-19 16:11:09 +0000

    Release 1.14.0

--- Formula/blackbird.rb
+++ Formula/blackbird.rb
@@ -3,7 +3,7 @@
 class Blackbird < Formula
   desc "Ambassador Blackbird is an API management and development tool that supercharges your ability to develop, test, mock, and deploy API services."
   homepage "https://www.getambassador.io/products/blackbird/api-development"
-  version "1.13.5"
+  version "1.14.0"
 
   BASE_URL = "https://blackbird.a8r.io/api/download/cli"
   ARCH = Hardware::CPU.arm? ? "arm64" : "amd64"
@@ -12,10 +12,10 @@ class Blackbird < Formula
 
   url "#{BASE_URL}/v#{version}/#{OPERATING_SYSTEM}/#{ARCH}"
 
-  sha256 "bdb2d10a4892aa5cd41f9f5edb9de5ecd3734a8392dfa8dce989ff35fd85079d" if OS.mac? && Hardware::CPU.intel?
-  sha256 "cac34ca0995e5f4dd2ca5d59a3143e4008871fc90afd8c0d4d1cfb6bc7ff848b" if OS.mac? && Hardware::CPU.arm?
-  sha256 "82dbaac349b459f0711d267a3a27dd20f340a11421be5f84881034cb8e7f1627" if OS.linux? && Hardware::CPU.intel?
-  sha256 "429a87e800236d7c0cd50df89b1adfd78faeb4f5296261321c031a367500f3b3" if OS.linux? && Hardware::CPU.arm?
+  sha256 "0a9715009d3d834fa79aae51ce1b0d6b41d7f241fc0a6619013269521f4b1f57" if OS.mac? && Hardware::CPU.intel?
+  sha256 "46889ecd729122ad324bc5a59797c03506220dcd40675db8e35481899607e303" if OS.mac? && Hardware::CPU.arm?
+  sha256 "bd7bf2919016e6bbacfcba2e2d759904bbaef68db242542f63c9e97bfdd218ea" if OS.linux? && Hardware::CPU.intel?
+  sha256 "ad3adf9e38ec85713f6845640dcb9a32b176e13751bf058263e42cbfa5c115ea" if OS.linux? && Hardware::CPU.arm?
 
   def install
       bin.install "#{PACKAGE_NAME}" => "blackbird"
